519b09a6ec5e16d9e92e12848dc09dca014af176
Berker Peksag <berker.peksag@gmail.com>
2012-08-07 05:29:57 +0300

    Add setup script.

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+# Taken from kennethreitz/requests/setup.py
+package_directory = os.path.realpath(os.path.dirname(__file__))
+
+def get_file_contents(file_path):
+    """Get the context of the file using full path name."""
+    full_path = os.path.join(package_directory, file_path)
+    return open(full_path, 'r').read()
+
+setup(
+    name='urlparse2',
+    version='0.1',
+    description='urlparse2 is a tiny wrapper for Python\'s urlparse module.',
+    long_description=get_file_contents('PYPI.rst'),
+    author='Berker Peksag',
+    author_email='berker.peksag@gmail.com',
+    url='https://github.com/berkrpeksag/urlparse2',
+    py_modules=['urlparse2'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    license='Mozilla Public License, v. 2.0',
+    classifiers=(
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+    test_suite='nose.collector',
+)
+

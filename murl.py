f02b96c17104e7a84159b6cc5cbed85f1df34677
Berker Peksag <berker.peksag@gmail.com>
2013-08-03 15:48:40 +0300

    Bump version to 0.3.2.

--- murl.py
+++ murl.py
@@ -11,7 +11,7 @@ except ImportError:
 
 __all__ = ['Url']
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 #: Parts for RFC 3986 URI syntax
 #: <scheme>://<netloc>/<path>;<params>?<query>#<fragment>

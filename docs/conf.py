e4ff41bd402a0b6fd2a5380c0b40f1c7cfb0be70
Berker Peksag <berker.peksag@gmail.com>
2014-09-17 06:00:43 +0300

    Update version info in docs.

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ copyright = u'2013, Berker Peksag'
 # built documents.
 #
 # The short X.Y version.
-version = '0.5'
+version = '0.5.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 

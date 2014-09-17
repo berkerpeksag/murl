ce879b50bacd463fd4ba66e4a89c70ed37c99c08
Berker Peksag <berker.peksag@gmail.com>
2014-09-17 06:01:19 +0300

    Cleanup docs/conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-
-# Add any Sphinx extension module names here, as strings. They can be extensions
-# coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
-
-# The encoding of source files.
-#source_encoding = 'utf-8-sig'
 
 # The master toctree document.
 master_doc = 'index'
